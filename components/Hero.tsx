a4bb7d51ba4a5d1f367c59fc55c45e8e334b0d97
audunu <audunu@gmail.com>
2022-05-16 10:33:24 +0200

    nothing

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 
 function Hero() {
-  return <div className='absolute h-screen xl:w-[1024px] w-[1024px] z-[-1]'>
-      <video autoPlay loop muted className="object-fill">
+  return <div className='absolute h-screen w-[1024px] z-[-1]'>
+      {/* <video autoPlay loop muted className="object-fill">
           <source src="/promovideo2.mp4" type="video/mp4" />
-        </video>
-    {/* <img src="/background2.jpeg" alt="" className="w-auto max-w-[100%]" /> */}
+        </video> */}
+    <img src="/background2.jpeg" alt="" className="w-auto max-w-[100%]" />
 
   </div>
 }
